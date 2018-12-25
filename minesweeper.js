8eccf38245348995be28da11a2f6d68cdceaa35b
Lili Boxer <ninaliboxer@gmail.com>
2018-12-25 09:06:57 -0800

    blank line

--- minesweeper.js
+++ minesweeper.js
@@ -1,1 +1,6 @@
-const blankline = ' | | '
+const blankline = '  |   |  ';
+
+console.log('This is what an empty board would look like: ');
+console.log(blankline);
+console.log(blankline);
+console.log(blankline);
