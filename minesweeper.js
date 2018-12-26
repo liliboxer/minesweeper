f2d3aca870d4e152a2422e23ce51544d0b6c40e8
Lili Boxer <ninaliboxer@gmail.com>
2018-12-26 08:04:14 -0800

    bomb board placemenet function

--- minesweeper.js
+++ minesweeper.js
@@ -1,3 +1,5 @@
+// Player Board //
+
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   const board = [];
   for (let i = 0; i < numberOfRows; i++) { // why don't you need .length
@@ -10,4 +12,28 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   return board;
 };
 
-console.log(generatePlayerBoard(5, 8));
+// Bomb Board //
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  const board = [];
+  for (let i = 0; i < numberOfRows; i++) {
+    const row = [];
+    for (let j = 0; j < numberOfColumns; j++) {
+      row.push(null);
+    };
+      board.push(row);
+  };
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced > numberOfBombs) { // can plce bombs on top of already existing bombs
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced ++;
+  };
+
+
+
+
+
+  return board;
+};
