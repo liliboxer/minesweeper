22c5bf0259af6a66940262485a999a80f2e92ee1
Lili Boxer <ninaliboxer@gmail.com>
2018-12-26 14:52:29 -0800

    game working!

--- minesweeper.js
+++ minesweeper.js
@@ -24,9 +24,9 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
       board.push(row);
   };
   let numberOfBombsPlaced = 0;
-  while (numberOfBombsPlaced > numberOfBombs) { // can plce bombs on top of already existing bombs
-    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
-    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+  while (numberOfBombsPlaced < numberOfBombs) { // can plce bombs on top of already existing bombs
+    const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
     board[randomRowIndex][randomColumnIndex] = 'B';
     numberOfBombsPlaced ++;
   };
@@ -36,6 +36,15 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
 
 // printBoard function //
 
-const printBoard = board => {
-  console.log(board.map(row => row.join(' | ').join(''\n''))); // don't understand syntax for thsi callback function
+let printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n')); // don't understand syntax for thsi callback function
 };
+
+
+// Playing the Game //
+
+const playerBoard = generatePlayerBoard(3,4);
+const bombBoard = generateBombBoard (3, 4, 5);
+
+printBoard(playerBoard);
+printBoard(bombBoard);
